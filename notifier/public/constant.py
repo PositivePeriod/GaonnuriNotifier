d9d1984baca3e8641bfd2624139bfcdd52ab05a0
PositivePeriod <positiveperiod.dev@gmail.com>
2021-03-06 04:01:28 +0900

    feat: organize static data

--- notifier/public/constant.py
+++ notifier/public/constant.py
@@ -1,31 +1,40 @@
 reply = dict()
 
-reply["success"] =
-'''<?xml version="1.0" encoding="UTF-8"?>
+reply["success"] = \
+    '''<?xml version="1.0" encoding="UTF-8"?>
 <response>
 <error>0</error>
 <message>success</message>
 <message_type></message_type>
 </response>'''
 
-reply["wrongID"] =
-'''<?xml version="1.0" encoding="UTF-8"?>
+
+reply["wrongID"] = \
+    '''<?xml version="1.0" encoding="UTF-8"?>
 <response>
 <error>-1</error>
 <message>존재하지 않는 회원 아이디입니다.</message>
 <message_type></message_type>
 </response>'''
 
-reply["wrongPW"] =
-'''<?xml version="1.0" encoding="UTF-8"?>
+reply["wrongPW"] = \
+    '''<?xml version="1.0" encoding="UTF-8"?>
 <response>
 <error>-1</error>
 <message>잘못된 비밀번호입니다.</message>
 <message_type></message_type>
 </response>'''
 
-requestLogin = lambda user_id, user_pw:
-f'''<?xml version="1.0" encoding="utf-8" ?>
+reply["errorOccur"] = \
+    '''<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL was not found on this server.</p>
+</body></html>'''
+
+requestLogin = lambda user_id, user_pw: f'''<?xml version="1.0" encoding="UTF-8" ?>
 <methodCall><params>
 <user_id>{user_id}</user_id>
 <password>{user_pw}</password>
@@ -35,32 +44,33 @@ f'''<?xml version="1.0" encoding="utf-8" ?>
 
 def welcome(language, userName):
     if language == 'ko':
-        return f'{user_name}님 환영해요'
-    if language == 'en':
-        return f'Welcome {user_name}'
+        return f'{userName}님 환영해요'
+    elif language == 'en':
+        return f'Welcome {userName}'
     else:
-        logger.warning(f'Impossible language | {language}')
-        return f'{user_name}'
+        logger.warning(f'Impossible language - {language}')
+        return f'{userName}'
 
 
 def isNewNotice(language, board_name):
     if language == 'ko':
-        return f'{board_name}에 새로운 공지가 올라왔어요'
-    if language == 'en':
+        return f'{board_name}의 새로운 공지'
+    elif language == 'en':
         return f'New notice from {board_name}'
     else:
-        logger.warning(f'Impossible language | {language}')
+        logger.warning(f'Impossible language - {language}')
         return f'{board_name}'
 
 
 def isNewNotNotice(language, board_name):
-        if language == 'ko':
-        return f'{board_name}에 새로운 글이 올라왔어요'
-    if language == 'en':
+    if language == 'ko':
+        return f'{board_name}의 새로운 글'
+    elif language == 'en':
         return f'New post from {board_name}'
     else:
-        logger.warning(f'Impossible language | {language}')
+        logger.warning(f'Impossible language - {language}')
         return f'{board_name}'
+
 
 def header(hostUrl, refererUrl):
     return {'Host': hostUrl[8:], 'Referer': refererUrl, 'Cache-Control': 'max-age=0'}
